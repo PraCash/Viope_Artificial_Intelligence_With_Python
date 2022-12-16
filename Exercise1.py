fb708a2f2b43cbab17f41c6146e81131453279de
PraCash <hello_sigdel@outlook.com>
2022-12-16 16:06:58 +0200

    Exercise Defalut Parameters

---
+++ Exercise1.py
@@ -0,0 +1,13 @@
+ 
+# This exercise tests out the default values of parameters. Create a program which has a main function and a subfunction called tester. The main function prompts user for an input "Write something (quit ends): " and sends this input to the subfunction as a parameter.
+#Define the subfunction tester so that it has one parameter called "givenstring", which has the default value "Too short". If the user input is less than 10 characters, the program uses the default value and if 10 or more, it prints the usergiven input. If the user inputs "quit", the program is terminated. When working correctly, the program will print out something like this:
+
+ 
+while True:
+    user_input = input("Write something (quit ends): ")
+    if user_input == "quit" :
+        break
+    if len(user_input) <= 5:
+        print("Too Short")
+    else:
+        print(user_input)
